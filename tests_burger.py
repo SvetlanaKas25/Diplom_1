be89c36d396a69cff0bb360f59d6d241ae2aac45
Svetlana Kasianova <SvetikSVDN@yandex.ru>
2026-06-23 19:32:42 +0300

    Добавлен тест test_move_ingredient в tests_burger.py

--- tests_burger.py
+++ tests_burger.py
@@ -55,3 +55,29 @@ class TestBurger:
         assert len(burger.ingredients) == 0
         assert mock_ingredient not in burger.ingredients
 
+
+    # Тест на перемещение ингредиента
+    @pytest.mark.parametrize("old_idx, new_idx, expected_names", [
+        (0, 1, ['chili sauce', 'cutlet']),
+        (1, 0, ['chili sauce', 'cutlet']),
+        (0, 0, ['cutlet', 'chili sauce']),  # перемещение на ту же позицию
+        (1, 1, ['cutlet', 'chili sauce']),  # перемещение на ту же позицию
+       ])
+    def test_move_ingredient(self, old_idx, new_idx, expected_names):
+        burger = Burger()
+        
+        mock_filling = Mock()
+        mock_filling.get_name.return_value = 'cutlet'
+
+        mock_sauce = Mock()
+        mock_sauce.get_name.return_value = 'chili sauce'
+        
+        burger.add_ingredient(mock_filling)
+        burger.add_ingredient(mock_sauce)
+        
+        burger.move_ingredient(old_idx, new_idx)
+        
+        actual_names = [ingredient.get_name() for ingredient in burger.ingredients]
+        
+        assert actual_names == expected_names
+
